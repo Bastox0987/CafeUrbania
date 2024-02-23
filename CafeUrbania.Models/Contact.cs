8d48cd9a9d03ea40b05b52e1341636caab3328bc
mbastor <mamounebastor7@gmail.com>
2024-02-23 14:11:23 -0500

    Ajoutez des fichiers projet.

---
+++ CafeUrbania.Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CafeUrbania.Models;
+
+public class Contact
+{
+    [Required]
+    public string Nom { get; set; }
+
+    public string Telephone { get; set; }
+
+    [Required]
+    public string Courriel { get; set; }
+
+    [Required]
+    public string Message { get; set; }
+
+    public DateTime DateHeureCreation { get; set; }
+}
